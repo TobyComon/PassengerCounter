ccf818e225010f19b18ac9ef60112adb18d2629d
Toby Comon <toby.comon@gmail.com>
2022-03-10 15:55:29 -0700

    Test

---
+++ Scrimba/PassengerCounter/index.js
@@ -0,0 +1,25 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let username = "Toby"
+let message = "You have three new notifications"
+let messageToUser = message + ", " + username + "!"
+
+
+console.log(messageToUser)
+
+
+//console.log(message + ", " + username + "!")
+
+function increment(){
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    
+    console.log(count)
+}
+    
